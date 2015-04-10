1037c20edc078a87a87a8bd27cbd67c2077aafd0
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-04-10 15:02:27 -0430

    se agrego la configuracion de gulp para el proyecto

---
+++ gulpfile.js
@@ -0,0 +1,114 @@
+var browserSync = require('browser-sync');
+var concat      = require('gulp-concat');
+var del         = require('del');
+var gulp        = require('gulp');
+var minifyCSS   = require('gulp-minify-css');
+var minifyHTML  = require('gulp-minify-html');
+var minifyJS    = require('gulp-uglify');
+var reload      = browserSync.reload;
+var rename      = require('gulp-rename');
+var stylus      = require('gulp-stylus');
+var nib         = require('nib');
+var jshint      = require('gulp-jshint');
+
+
+// MANAGER USER
+
+var _LIBS = [
+    'libs/**/*.js',
+    'libs/**/*.js.map'
+];
+
+var _IMGS = [
+    'core/media/**/*.png',
+    'media/**/*.png'
+];
+
+
+// WARNING CORE
+
+var _CORE = [
+    'core/class/**/*.js'
+];
+
+var _HTML = [
+    'core/static/**/*.html'
+];
+
+var _STYLUS = 'assets/stylus/**/*.styl';
+
+gulp.task('minify-css', function () {
+    gulp.src(_STYLUS)
+    .pipe(concat('style.min.styl'))
+    .pipe(stylus({ use: nib(),  import: ['nib']}))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('game/assets/css'))
+    .pipe(reload({stream: true, once: true}));
+});
+
+gulp.task('minify-js', function () {
+    gulp.src(_CORE)
+    .pipe(concat('main.min.js'))
+    .pipe(minifyJS())
+    .pipe(gulp.dest('game/assets/js'))
+    .pipe(reload({stream: true, once: true}));
+});
+
+gulp.task('lint', function () {
+    gulp.src(_CORE)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('minify-html', function () {
+    gulp.src(_HTML)
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('game'))
+    .pipe(reload({stream: true, once: true}));
+});
+
+gulp.task('copyJs', function(){
+	gulp.src(_LIBS)
+    .pipe(gulp.dest('game/assets/js'));
+});
+
+
+gulp.task('copyImgs', function(){
+	gulp.src(_IMGS)
+    .pipe(gulp.dest('game/assets/img'));
+});
+
+
+gulp.task('watch', function() {
+    // Cambios principales
+    gulp.watch(_CORE, ['lint', 'minify-js']);
+    gulp.watch(_STYLUS,      ['minify-css']);
+    gulp.watch(_HTML,       ['minify-html']);
+});
+
+gulp.task('server-start', function () {
+    browserSync({
+        notify: false,
+        server: 'game/'
+    });
+});
+
+
+//Tareas IMPORTANTES
+
+gulp.task('debug', function() {
+    gulp.start('lint');
+});
+
+gulp.task('dist', ['clean'], function() {
+    gulp.start('copyJs', 'copyImgs', 'minify-css', 'minify-html', 'minify-js', 'lint');
+});
+
+gulp.task('server', function() {
+    gulp.start('lint', 'server-start', 'watch');
+});
+
+gulp.task('clean', function(cb) {
+    del(['game'], cb);
+});
