e03b11b71de0329d64253dbfecfe58becd09d1f3
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-04-13 21:57:46 -0430

    se agrego el manejo del objeto canvas mostrando un conteo de los ciclos en el gameloop

---
+++ core/class/core.js
@@ -0,0 +1,33 @@
+(function(){
+
+   //METODOS PRIVADOS
+
+   function Static(){
+      var canvas      = document.getElementById('game');
+      var context     = canvas.getContext('2d');
+      var cleanCanvas = function(){
+         context.clearRect(0, 0, canvas.width, canvas.height);
+      };
+      return {
+         canvas      : canvas,
+         context     : context,
+         cleanCanvas : cleanCanvas
+      };
+   }
+
+
+   //METODOS PUBLICOS
+
+   window.core = function(){
+      this.count = 0;
+      console.info('Iniciando...');
+   };
+
+   window.core.prototype.debug = function(){
+      Static().cleanCanvas();
+      Static().context.fillStyle = 'red';
+      Static().context.font = 'bold 12px sans-serif';
+      Static().context.fillText(this.count++, 9, 20);
+   };
+
+})();
--- core/class/gameLoop.js
+++ core/class/gameLoop.js
@@ -1,24 +1,22 @@
-(function(){
-   // Creando GameLoop {
-   var getRequestAnimationFrame = function () {
-   return window.requestAnimationFrame ||
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame ||
-          window.oRequestAnimationFrame ||
-          window.msRequestAnimationFrame ||
-          function (callback){
-            window.setTimeout(callback, 1000/60);
-          };
-   };
+window.onload = function(){
+  // Creando GameLoop {
+  var getRequestAnimationFrame = function () {
+  return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback){
+      window.setTimeout(callback, 1000/60);
+    };
+  };
+  var game = new core();
+  var fpAnimationFrame = getRequestAnimationFrame();
+  var gameLoop = function () {
+    game.debug();
+    fpAnimationFrame(gameLoop);
+  };
 
-   var count = 0;
-   var fpAnimationFrame = getRequestAnimationFrame();
-   var gameLoop = function () {
-      count++;
-      console.info(count);
-      fpAnimationFrame(gameLoop);
-   };
-
-   fpAnimationFrame(gameLoop);
-   // Termino GameLoop } 
-})();
+  fpAnimationFrame(gameLoop);
+  // Termino GameLoop } 
+};
