1700d7171f272f91660afa6aa1d86f8b1387e8dc
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-04-29 12:50:03 -0430

    trabajando el modulo loading

--- core/class/modules/fps.js
+++ core/class/modules/fps.js
@@ -1,16 +1,16 @@
-(function(){
+// (function(){
 
-    devGameJs.addModule("fps", function (binding) {
-        var currentFps = 0,
-            frameCount = 0,
-            lastFps = new Date().getTime(),
-            oBuffer = binding.canvas.bufferContext;
+//     devGameJs.addModule("fps", function (binding) {
+//         var currentFps = 0,
+//             frameCount = 0,
+//             lastFps = new Date().getTime(),
+//             oBuffer = binding.canvas.bufferContext;
      
-        return {
-            init : function () {
-            	binding.state.set(1);
-            }
-        };
-    });
+//         return {
+//             init : function () {
+//             	binding.state.set(1);
+//             }
+//         };
+//     });
 
-})();
+// })();
---
+++ core/class/modules/loading.js
@@ -0,0 +1,39 @@
+(function(){
+   devGameJs.addModule("loading", function (binding) {
+      
+      var imgs;
+      var imageCount;
+      var loadCount;
+      var errorCount;
+
+      //VARIABLES DE CONTROL
+      var isLoading     = true;
+      var isSuccessful  = false;
+      var percentLoaded = 0;
+
+      var states = {
+         PENDING  : 1,
+         LOADING  : 2,
+         RESOLVED : 3,
+         REJECTED : 4
+      };
+
+      var state = states.PENDING;
+
+      //Metodos Privados
+      return {
+         init : function () {
+            binding.state.set(1);
+         },
+         load : function(){
+            if (isInitiated())
+               return;
+            state  = states.LOADING;
+            for (var i = 0 ; i < imageCount ; i++) {
+               loadImageLocation(imgs[i]);
+            }
+         }
+      };
+   });
+
+})();
