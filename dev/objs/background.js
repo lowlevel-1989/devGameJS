b6ae352d16c78adeb03292dd0373ec9f8f62aceb
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-06-28 03:19:09 -0500

    se limpio el codigo

--- dev/objs/background.js
+++ dev/objs/background.js
@@ -9,8 +9,10 @@
    background.layer    = 0;
 
    background.draw = function(canvas){
-      canvas.bufferContext.fillStyle = '#000';
+      //Cuadricula de fondo
       canvas.bufferContext.drawImage(this.sprite, 0, 0, this.width, this.height);
+      //Area para el fps
+      canvas.bufferContext.fillStyle = '#000';
       canvas.bufferContext.fillRect(0, 0, 150, 40);
       
    };
