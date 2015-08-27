d050c1cca8f711e8848e4ccb918f7a813f813eb5
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-08-27 08:04:17 -0500

    se mejoro rendimiento de la consola

--- core/source/_console.js
+++ core/source/_console.js
@@ -1,7 +1,9 @@
 var debug = document.createElement('div');
+var max   = 7;
+var count = 0;
 
 debug.style.width      = '400px';
-debug.style.height     = '300px';
+debug.style.height     = '200px';
 debug.style.position   = 'absolute';
 debug.style.right      = '0px';
 debug.style.padding    = '10px';
@@ -10,29 +12,24 @@ debug.style.color      = '#FFF';
 debug.style.overflow   = 'hidden';
 debug.style.cursor     = 'default';
 
-var count = 0;
-
 if (!navigator.isCocoonJS){
    window.console.info = window.console.log;
    window.console.log = function(log, clear){
-      var isObject = typeof log === 'object';
-      
-      if (++count > 20){
-         count = 0;
-         clear = true;
-      }
-
-      if (clear)
-         debug.innerHTML = '';
+      var isObject  = typeof log === 'object';
+      count++;
 
       if (isObject){
          debug.innerHTML = 'width: ' + log.width  + ' height: ' + log.height +'<br/>' + debug.innerHTML;
          debug.innerHTML = 'y: ' + log.y  + ' vy: ' + log.vy +'<br/>' + debug.innerHTML;
          debug.innerHTML = 'x: ' + log.x  + ' vx: ' + log.vx +'<br/>' + debug.innerHTML;
          debug.innerHTML = 'Object id: '+ log.id + '<br/>' + debug.innerHTML;
-         count+=4;
       }else
          debug.innerHTML = log + '<br/>' + debug.innerHTML;
+
+      if (count === max){
+         debug.innerHTML = '';
+         count=0;
+      }
 
    };
 }else
