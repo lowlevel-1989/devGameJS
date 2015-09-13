c80d2668462764f1417856dfbc46f29bfcb94d65
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-09-12 22:29:38 -0500

    se agregaron test de gravedad y metodos devGameJs

---
+++ test/spec/devGameJs.methods.js
@@ -0,0 +1,49 @@
+describe('devGameJs', function(){
+
+   it('Se creo la class devGameJs?', function(){
+      
+      var response = devGameJs ? true : false;
+      expect(response).toBe(true);
+
+      if (response){
+
+         describe('devGameJs.methods', function(){
+
+            it('Existe el metodo startGame?', function(){
+               expect('startGame' in devGameJs).toBe(true);
+            });
+
+            it('Existe el metodo objects?', function(){
+               expect('objects' in devGameJs).toBe(true);
+            });
+
+            it('Existe el metodo addObject?', function(){
+               expect('addObject' in devGameJs).toBe(true);
+            });
+
+            it('Existe el metodo random?', function(){
+               expect('random' in devGameJs).toBe(true);
+            });
+
+            it('Existe el metodo module?', function(){
+               expect('module' in devGameJs).toBe(true);
+            });
+            
+            it('Existe el metodo addModule?', function(){
+               expect('addModule' in devGameJs).toBe(true);
+            });
+
+            it('Existe el metodo setup?', function(){
+               expect('setup' in devGameJs).toBe(true);
+            });
+
+            it('Existe el metodo version?', function(){
+               expect('version' in devGameJs).toBe(true);
+            });
+            
+         });
+      }
+
+   });
+
+});
