8b64f8ee589cc57a2cd4ee6d4d29e982c53ea443
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-09-12 21:10:29 -0500

    se agrego el comando test a gulp

--- test/spec/newObjects.js
+++ test/spec/newObjects.js
@@ -1,10 +1,21 @@
-describe("newObjects", function() {
+describe('DevGameJs', function(){
 
-   it("Esta funcionando el framework?", function() {
+   it('Se creo la class devGameJs?', function(){
       
       var response = devGameJs ? true : false;
-
       expect(response).toBe(true);
+
+      if (response){
+
+         describe('objects', function(){
+
+            it('Existe el metodo objects?', function(){
+               expect('objects' in devGameJs).toBe(true);
+            });
+
+         });
+      }
+
    });
 
 });
