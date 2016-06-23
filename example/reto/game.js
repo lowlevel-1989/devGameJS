fd5afe55046981f038df4f41ef1ea4dd4866ed29
FormatCom <formatcomvinicio@gmail.com>
2016-06-23 14:38:58 -0500

    se agregaron funciones extras al teclado

--- example/reto/game.js
+++ example/reto/game.js
@@ -3,6 +3,7 @@
 
   var canvas  = document.getElementById('game')
   var context = canvas.getContext('2d')
+  context.font      = 'normal 16px monospace'
 
   var debugRefresh = 400
 
@@ -11,6 +12,15 @@
 
   var _seg = 0
   var _fps = 0
+
+  var _debug     = false
+  var _info      = true
+  var _preview   = false
+
+  var KEY_F1 = 112
+  var KEY_F2 = 113
+  var KEY_F3 = 114
+  var KEY_F4 = 115
 
   var size  = 60
   var rows  = 8
@@ -22,6 +32,7 @@
 
   var buffer = document.createElement('canvas')
   buffer._pos   = Math.sqrt((size*2)+(size*2))
+  buffer.style.display = 'none'
   
   document.body.appendChild(buffer)
 
@@ -35,7 +46,8 @@
 
   var bufferContext = buffer.getContext('2d')
   bufferContext.fillStyle = '#000'
-  bufferContext.font      = 'normal 16pt Arial'
+  bufferContext.font      = 'normal 16pt monospace'
+
 
   var mousex    = null
   var mousey    = null
@@ -118,9 +130,11 @@
       bufferContext.drawImage(spritesheet, this.sx, this.sy, this.width, this.height, -this.width/2, -this.height/2, this.width, this.height)
     }
     bufferContext.strokeRect(-this.width/2, -this.height/2, this.width, this.height)
-    bufferContext.fillStyle = this.color2
-    bufferContext.textAlign = 'center'
-    bufferContext.fillText(this.id, 0, 7)
+    if (_debug){
+      bufferContext.fillStyle = this.color2
+      bufferContext.textAlign = 'center'
+      bufferContext.fillText(this.id, 0, 7)
+    }
     bufferContext.rotate(-_rotation)
     bufferContext.translate(-_x, -_y)
 
@@ -151,7 +165,9 @@
   Cell.prototype.logic = function(){
     if (this.piece){ 
 
-      this.sx -= Math.sin(pieces.change*DEVGAME.DEG_TO_RAD)
+      if (pieces.animation){
+        this.sx -= Math.sin(pieces.change*DEVGAME.DEG_TO_RAD)
+      }
       this.animation()
 
       if (mouse.down === 1 && mouse.dragging === false){
@@ -165,7 +181,11 @@
         if (DEVGAME.collision.rectToCircle(this, mouse)){
           this.select = mouse.dragging = true
           mask.set(mouse.x-(mask.width/2), mouse.y-(mask.height/2))
-          mask.visible = true
+          if (_debug){
+            mask.visible = true
+          }else{
+            mask.visible = false
+          }
         }
 
         this.x = _x
@@ -235,6 +255,7 @@
   var dragging = new Dragging(0, 0, size, true, '#000', '#FFF')
 
   var pieces = new DEVGAME.Container() 
+  pieces.animation = true
 
   for (var y = 0; y < rows; y++){
     for (var x = 0; x < cols; x++){
@@ -276,18 +297,46 @@
     //clear canvas
     context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight)
 
-    pieces.change -= 1.2
+    if (_info){
+
+      context.textAlign = 'left'
+      context.fillText( 'FPS: '+ _fps, 10, 16 )
+      context.fillText( 'F1 : DEBUG TRUE/FALSE',  10, 32 )
+      context.fillText( 'F2 : SHOW/HIDE INFO',  10, 48 )
+      context.fillText( 'F3 : ON/OFF ANIMATION',  10, 64 )
+      context.fillText( 'F4 : SHOW/HIDE IMAGE PREVIEW',  10, 80 )
+
+      
+      context.textAlign = 'center'
+      context.fillText('CREDITS', canvas.clientWidth-140, canvas.clientHeight - 64)
+      context.fillText('Developer: FormatCom', canvas.clientWidth-140, canvas.clientHeight - 48)
+      context.fillText('Graphic Design: Vickyg10', canvas.clientWidth-140, canvas.clientHeight - 32)
+      context.fillText('Thanks: Juegos Canvas Ninja', canvas.clientWidth-140, canvas.clientHeight - 16)
+    }
+
+    if (_preview){
+      context.strokeRect((canvas.clientWidth-spritesheet.width/1.8) - 45, 10, spritesheet.width/1.8, spritesheet.height/1.8)
+      context.drawImage(spritesheet, 0, 0, spritesheet.width, spritesheet.height, (canvas.clientWidth-spritesheet.width/1.8) - 45, 10, spritesheet.width/1.8, spritesheet.height/1.8)
+    }
+
+    if (pieces.animation){
+      pieces.change -= 1.2
+    }
+
     stage.exec()
     stage.render()
 
     //clear mouse
     mouseDown = mouseUp = null
 
-    //draw fps
-    context.textAlign = 'left'
-    context.fillStyle = '#000'
-    context.font      = 'normal 16pt Arial'
-    context.fillText( 'FPS: '+ _fps, 10, 20 )
+
+    if (_debug){
+      buffer.style.display = 'block'
+      mouse.visible = true
+    }else{
+      buffer.style.display = 'none'
+      mouse.visible = false
+    }
 
     exec(loop)
 
@@ -314,6 +363,51 @@
 
   canvas.addEventListener('mouseup',function(event){
     mouseUp = event.which
+  }, false)
+
+  canvas.addEventListener('touchstart',function(event){
+    event.preventDefault()
+    mouseDown = 1
+
+    mousex = event.targetTouches[0].pageX - canvas.offsetLeft
+    mousey = event.targetTouches[0].pageY - canvas.offsetTop
+  }, false)
+
+  canvas.addEventListener('touchmove',function(event){
+    event.preventDefault()
+
+    mousex = event.targetTouches[0].pageX - canvas.offsetLeft
+    mousey = event.targetTouches[0].pageY - canvas.offsetTop
+  }, false)
+
+  canvas.addEventListener('touchend',function(event){
+    mouseUp = 1
+  }, false)
+
+  canvas.addEventListener('touchcancel',function(event){
+    mouseUp = 1
+  }, false)
+
+  document.addEventListener('keydown', function(event){
+    event.preventDefault()
+  }, false)
+
+  document.addEventListener('keyup', function(event){
+    event.preventDefault()
+    if (event.keyCode === KEY_F1){
+      _debug = !_debug
+    }
+    if (event.keyCode === KEY_F2){
+      _info = !_info
+    }
+    if (event.keyCode === KEY_F3){
+      pieces.animation = !pieces.animation
+    }
+    if (event.keyCode === KEY_F4){
+      _preview = !_preview
+    }
+
+
   }, false)
 
 })(window, document, DEVGAME)
